f2f363eed5f2054deb4ca1fa812b4e42de94a17a
Divyanshu Rathour <divyanshu.rathore0522@gmail.com>
2025-06-01 21:01:50 +0530

    Rent Pey Lo

--- rent_pay_lo/src/App.jsx
+++ rent_pay_lo/src/App.jsx
@@ -1,15 +1,18 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+// App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './login_page'
 import HomeScreen from './home_page'
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/home' element={<HomeScreen />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
+
+export default App;
